780cea18bcff03011ce2906720168b650ba1e9d2
hyewonShin <67300338+hyewonShin@users.noreply.github.com>
2024-12-15 22:14:36 +0900

    feat: 버튼 클릭 시 ChatRoomCreateTest으로 연결(임시)

--- lib/ui/widgets/create_chat_room_button.dart
+++ lib/ui/widgets/create_chat_room_button.dart
@@ -1,15 +1,17 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dongne_chat/ui/pages/dongne_list/chat_room_creat_test.dart';
 import 'package:flutter/material.dart';
 
 class CreateChatRoomButton extends StatelessWidget {
   /// 채팅방 생성 플로팅 버튼
-  const CreateChatRoomButton({super.key});
+  CreateChatRoomButton({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // TODO: 기능 구현 필요
     return FloatingActionButton(
       onPressed: () {
-        print('onPress');
+        Navigator.push(context,
+            MaterialPageRoute(builder: (context) => ChatRoomCreateTest()));
       },
       shape: CircleBorder(),
       backgroundColor: Theme.of(context).highlightColor,
