777fb6256103ac1c6fe40ad5b98c1abe7693b9f8
hyewonShin <67300338+hyewonShin@users.noreply.github.com>
2024-12-15 22:12:34 +0900

    feat: 동네리스트아이템에 firebase의 chatRoom 컬렉션 데이터 출력

--- lib/ui/pages/dongne_list/widgets/dongne_list_item.dart
+++ lib/ui/pages/dongne_list/widgets/dongne_list_item.dart
@@ -1,47 +1,74 @@
 import 'package:dongne_chat/ui/pages/dongne_list/widgets/modal_bottom.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class DongneListItem extends StatelessWidget {
+  final roomId;
+  final title;
+  final info;
+  final category;
+  final createdAt;
+
   /// 채팅방 리스트
-  const DongneListItem({super.key});
+  // const DongneListItem({super.key});
+  const DongneListItem({
+    super.key,
+    required this.roomId,
+    required this.title,
+    required this.info,
+    required this.category,
+    required this.createdAt,
+  });
 
   @override
   Widget build(BuildContext context) {
+    // DateTime으로 파싱
+    DateTime parsedDate = DateTime.parse(createdAt.toString());
+
     return GestureDetector(
       onTap: () {
-        showModalBottom(context);
+        showModalBottom(context, roomId, title, info, category);
       },
-      child: Container(
-        height: 80,
-        width: double.infinity,
-        color: Colors.transparent,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              '채팅방 이름',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 8),
-            Row(
-              children: [
-                Expanded(
-                  child: Text(
-                    '마지막 채팅 내용 마지막 채팅 내용 마지막 채팅 내용 '
-                    '마지막 채팅 내용 마지막 채팅 내용',
-                    softWrap: false,
-                    overflow: TextOverflow.clip,
-                    style: TextStyle(fontSize: 16),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          height: 80,
+          width: double.infinity,
+          color: Colors.transparent,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    '$title',
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
                   ),
-                ),
-                Container(
-                  width: 100,
-                  alignment: Alignment.centerRight,
-                  child: Text('오후 8:50', style: TextStyle(fontSize: 16)),
-                ),
-              ],
-            )
-          ],
+                  Text(DateFormat('yy-MM-dd').format(parsedDate),
+                      style: TextStyle(fontSize: 13)),
+                ],
+              ),
+              SizedBox(height: 8),
+              Row(
+                children: [
+                  Expanded(
+                    child: Text(
+                      '$info',
+                      softWrap: false,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(fontSize: 16),
+                    ),
+                  ),
+                  // Container(
+                  //   alignment: Alignment.centerRight,
+                  //   child: Text(DateFormat('yy-MM-dd').format(parsedDate),
+                  //       style: TextStyle(fontSize: 13)),
+                  // ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
