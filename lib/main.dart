d8823b412fcb8d22b7b4ce75477db33a5f48ff1c
hyewonShin <67300338+hyewonShin@users.noreply.github.com>
2024-12-15 22:22:57 +0900

    feat: main() 에 ProviderScope 생성

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:dongne_chat/theme.dart';
+import 'package:dongne_chat/ui/pages/chat/chat_page.dart';
+import 'package:dongne_chat/ui/pages/dongne_list/dongne_list_page.dart';
 import 'package:dongne_chat/ui/pages/home/home_page.dart';
 import 'package:dongne_chat/ui/pages/login/login_page.dart';
+import 'package:dongne_chat/ui/pages/mypage/mypage.dart';
 import 'package:dongne_chat/ui/pages/signup/signup_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -13,7 +17,11 @@ void main() async {
   await dotenv.load(fileName: ".env");
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-  runApp(const MyApp());
+  runApp(
+    ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -23,7 +31,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: theme,
-      home: const SignupPage(),
+      home: LoginPage(),
     );
   }
 }
