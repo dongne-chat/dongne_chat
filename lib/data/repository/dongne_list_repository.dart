c56016c56f8ec2cbebd349b986918a4cde1f50b2
hyewonShin <67300338+hyewonShin@users.noreply.github.com>
2024-12-16 21:48:06 +0900

    feat: DongneListRepository 생성

---
+++ lib/data/repository/dongne_list_repository.dart
@@ -0,0 +1,43 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dongne_chat/data/model/chat_room.dart';
+
+class DongneListRepository {
+  // 채팅름 목록을 가져오는 스트림 메서드
+  Stream<List<ChatRoom>> getAllChatRooms() {
+    final firestore = FirebaseFirestore.instance;
+    final collectionRef = firestore.collection('chatRooms');
+
+    return collectionRef
+        .orderBy('createdAt', descending: true) // 내림차순 정렬
+        .snapshots()
+        .map((snapshot) {
+      return snapshot.docs.map((doc) => ChatRoom.fromJson(doc)).toList();
+    });
+  }
+
+  // 채팅룸 생성
+  Future<void> createChatRoom(
+      title, info, category, userId, addressName) async {
+    final firestore = FirebaseFirestore.instance;
+    final collectionRef = firestore.collection('chatRooms');
+
+    collectionRef.add({
+      'title': title,
+      'info': info,
+      'category': category,
+      'users': [userId], // 채팅방에 접속한 user_id 배열에 저장
+      'address': addressName, // 현재 좌표로 주소 찾아서 넣어주기
+      'createdAt': Timestamp.now()
+    });
+  }
+
+  // 채팅방 입장 시 user_id 삽입
+  Future<void> insertUserId(roomId, userId) async {
+    final firestore = FirebaseFirestore.instance;
+    final collectionRef = firestore.collection('chatRooms');
+
+    collectionRef.doc(roomId).update({
+      'users': FieldValue.arrayUnion([userId])
+    });
+  }
+}
