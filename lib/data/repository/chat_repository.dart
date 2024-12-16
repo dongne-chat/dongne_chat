898d04390b307064ea25f5d070b6ea61ccc2b229
hyewonShin <67300338+hyewonShin@users.noreply.github.com>
2024-12-16 22:09:44 +0900

    refactor: DongneListRepository, ChatRepository에 try~catch 예외처리

--- lib/data/repository/chat_repository.dart
+++ lib/data/repository/chat_repository.dart
@@ -4,29 +4,38 @@ import 'package:dongne_chat/data/model/chat_message.dart';
 class ChatRepository {
   // 채팅방 메세지 불러오기
   Stream<List<ChatMessage>> getAllChatMessages(roomId) {
-    final firestore = FirebaseFirestore.instance;
-    final collectionRef = firestore.collection('chatRooms').doc(roomId);
+    try {
+      final firestore = FirebaseFirestore.instance;
+      final collectionRef = firestore.collection('chatRooms').doc(roomId);
 
-    return collectionRef
-        .collection('messages')
-        .orderBy('createdAt', descending: true) // 최신 메시지부터 가져오기
-        .snapshots()
-        .map((snapshot) {
-      return snapshot.docs
-          .map((doc) => ChatMessage.fromJson(doc.id, doc.data()))
-          .toList();
-    });
+      return collectionRef
+          .collection('messages')
+          .orderBy('createdAt', descending: true) // 최신 메시지부터 가져오기
+          .snapshots()
+          .map((snapshot) {
+        return snapshot.docs
+            .map((doc) => ChatMessage.fromJson(doc.id, doc.data()))
+            .toList();
+      });
+    } catch (e) {
+      print(e);
+      return Stream.value([]);
+    }
   }
 
   // 채팅방 메세지 전송
   Future<void> createMessage(roomId, content, senderId) async {
-    final firestore = FirebaseFirestore.instance;
-    final collectionRef = firestore.collection('chatRooms');
+    try {
+      final firestore = FirebaseFirestore.instance;
+      final collectionRef = firestore.collection('chatRooms');
 
-    collectionRef.doc(roomId).collection('messages').add({
-      'content': content,
-      'senderId': senderId,
-      'createdAt': Timestamp.now(),
-    });
+      collectionRef.doc(roomId).collection('messages').add({
+        'content': content,
+        'senderId': senderId,
+        'createdAt': Timestamp.now(),
+      });
+    } catch (e) {
+      print(e);
+    }
   }
 }
--- lib/data/repository/dongne_list_repository.dart
+++ lib/data/repository/dongne_list_repository.dart
@@ -4,40 +4,53 @@ import 'package:dongne_chat/data/model/chat_room.dart';
 class DongneListRepository {
   // 채팅름 목록을 가져오는 스트림 메서드
   Stream<List<ChatRoom>> getAllChatRooms() {
-    final firestore = FirebaseFirestore.instance;
-    final collectionRef = firestore.collection('chatRooms');
+    try {
+      final firestore = FirebaseFirestore.instance;
+      final collectionRef = firestore.collection('chatRooms');
 
-    return collectionRef
-        .orderBy('createdAt', descending: true) // 내림차순 정렬
-        .snapshots()
-        .map((snapshot) {
-      return snapshot.docs.map((doc) => ChatRoom.fromJson(doc)).toList();
-    });
+      return collectionRef
+          .orderBy('createdAt', descending: true) // 내림차순 정렬
+          .snapshots()
+          .map((snapshot) {
+        return snapshot.docs.map((doc) => ChatRoom.fromJson(doc)).toList();
+      });
+    } catch (e) {
+      print(e);
+      return Stream.value([]);
+    }
   }
 
   // 채팅룸 생성
   Future<void> createChatRoom(
       title, info, category, userId, addressName) async {
-    final firestore = FirebaseFirestore.instance;
-    final collectionRef = firestore.collection('chatRooms');
+    try {
+      final firestore = FirebaseFirestore.instance;
+      final collectionRef = firestore.collection('chatRooms');
 
-    collectionRef.add({
-      'title': title,
-      'info': info,
-      'category': category,
-      'users': [userId], // 채팅방에 접속한 user_id 배열에 저장
-      'address': addressName, // 현재 좌표로 주소 찾아서 넣어주기
-      'createdAt': Timestamp.now()
-    });
+      collectionRef.add({
+        'title': title,
+        'info': info,
+        'category': category,
+        'users': [userId], // 채팅방에 접속한 user_id 배열에 저장
+        'address': addressName, // 현재 좌표로 주소 찾아서 넣어주기
+        'createdAt': Timestamp.now()
+      });
+    } catch (e) {
+      print(e);
+    }
   }
 
   // 채팅방 입장 시 user_id 삽입
   Future<void> insertUserId(roomId, userId) async {
-    final firestore = FirebaseFirestore.instance;
-    final collectionRef = firestore.collection('chatRooms');
+    try {
+      final firestore = FirebaseFirestore.instance;
+      final collectionRef = firestore.collection('chatRooms');
 
-    collectionRef.doc(roomId).update({
-      'users': FieldValue.arrayUnion([userId])
-    });
+      collectionRef.doc(roomId).update({
+        'users': FieldValue.arrayUnion([userId])
+      });
+    } catch (e) {
+      print(e);
+    }
   }
 }
