aaad6227e86690b524422581afa3d931d821e09a
hyewonShin <67300338+hyewonShin@users.noreply.github.com>
2024-12-16 21:58:33 +0900

    refactor: 사용되지 않는 import 제거

--- lib/ui/pages/dongne_list/widgets/category_list.dart
+++ lib/ui/pages/dongne_list/widgets/category_list.dart
@@ -1,7 +1,4 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:dongne_chat/data/model/chat_room.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class CategoryList extends StatefulWidget {
   final List<String> categories = [
