43a4818f6393d4ca18bf9692562ad6be05c0139e
hyewonShin <67300338+hyewonShin@users.noreply.github.com>
2024-12-17 16:47:59 +0900

    feat: ChatMessageWithUser 모델 생성 후, 채팅방에서 이미지/닉네임/메세지 동시 출력 되도록 구현

--- lib/data/repository/chat_repository.dart
+++ lib/data/repository/chat_repository.dart
@@ -1,9 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dongne_chat/data/model/chat_message.dart';
+import 'package:dongne_chat/data/model/user.dart';
 
 class ChatRepository {
   // 채팅방 메세지 불러오기
-  Stream<List<ChatMessage>> getAllChatMessages(roomId) {
+  Stream<List<ChatMessage>> getAllChatMessages(String roomId) {
     try {
       final firestore = FirebaseFirestore.instance;
       final collectionRef = firestore.collection('chatRooms').doc(roomId);
@@ -24,7 +25,8 @@ class ChatRepository {
   }
 
   // 채팅방 메세지 전송
-  Future<void> createMessage(roomId, content, senderId) async {
+  Future<void> createMessage(
+      String roomId, String content, String senderId) async {
     try {
       final firestore = FirebaseFirestore.instance;
       final collectionRef = firestore.collection('chatRooms');
@@ -37,5 +39,53 @@ class ChatRepository {
     } catch (e) {
       print(e);
     }
+  }
+
+  // user의 데이터 가져오는 메서드
+  Future<List<User?>> getUserData(String id) async {
+    try {
+      final firestore = FirebaseFirestore.instance;
+      final collectionRef = firestore.collection('users');
+      final result = await collectionRef.where('id', isEqualTo: id).get();
+
+      final docs = result.docs;
+      if (docs.isNotEmpty) {
+        return docs.map((doc) => User.fromMap(doc.data())).toList();
+      }
+      return [];
+    } catch (e) {
+      print(e);
+      return [];
+    }
+  }
+
+// roomId를 기준으로 채팅방 참여한 사람들의 id인 users 데이터 가져오기
+  Future<List<String>?> fetchUsersArray(String roomId) async {
+    final firestore = FirebaseFirestore.instance;
+    final collectionRef = firestore.collection('chatRooms');
+    final docSnapshot = await collectionRef.doc(roomId).get();
+
+    if (docSnapshot.exists) {
+      final chatUsers = List<String>.from(docSnapshot['users']);
+      return chatUsers;
+    }
+    return null;
+  }
+
+  // users에 있는 사용자의 정보 가져오기
+  Future<Map<String, User>?> fetchUsersData(List<String>? chatUsers) async {
+    final firestore = FirebaseFirestore.instance;
+    final collectionRef = firestore.collection('users');
+    final querySnapshot = await collectionRef
+        .where(FieldPath.documentId, whereIn: chatUsers)
+        .get();
+
+    final docs = querySnapshot.docs;
+    if (docs.isNotEmpty) {
+      final users = docs.map((doc) => User.fromMap(doc.data())).toList();
+      var userMap = {for (var user in users) user.id: user};
+      return userMap;
+    }
+    return null;
   }
 }
--- lib/ui/pages/chat/chat_view_model.dart
+++ lib/ui/pages/chat/chat_view_model.dart
@@ -1,14 +1,20 @@
-import 'package:dongne_chat/data/model/chat_message.dart';
+import 'package:dongne_chat/data/model/chat_message_with_user.dart';
+import 'package:dongne_chat/data/model/user.dart';
 import 'package:dongne_chat/data/repository/chat_repository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class ChatViewModel
-    extends AutoDisposeFamilyNotifier<List<ChatMessage>, String> {
+    extends AutoDisposeFamilyNotifier<List<ChatMessageWithUser>, String> {
   final chatMessageRepository = ChatRepository();
 
+  // users 데이터를 관리하기 위한 상태 추가
+  Map<String, User>? users;
+
   @override
-  List<ChatMessage> build(String roomId) {
-    fetchData(roomId);
+  List<ChatMessageWithUser> build(String roomId) {
+    fetchUsers(roomId).then((_) {
+      fetchData(roomId);
+    });
     return [];
   }
 
@@ -17,7 +23,25 @@ class ChatViewModel
     final stream = chatMessageRepository.getAllChatMessages(roomId);
     final streamSubscription = stream.listen(
       (chatMessageList) {
-        state = chatMessageList;
+        // 유저 정보와 메시지를 결합
+        final chatMessagesWithUserData = chatMessageList.map((chatMessage) {
+          final user =
+              users?[chatMessage.senderId]; // 메시지의 senderId로 유저 정보 가져오기
+          if (user != null) {
+            // 유저 정보가 있으면 메시지에 결합하여 리턴
+            return ChatMessageWithUser(
+              chatMessage: chatMessage,
+              user: user,
+            );
+          } else {
+            return ChatMessageWithUser(
+              chatMessage: chatMessage,
+              user: null,
+            );
+          }
+        }).toList();
+
+        state = chatMessagesWithUserData; // 상태 업데이트
       },
     );
     ref.onDispose(
@@ -31,9 +55,30 @@ class ChatViewModel
   Future<void> handleCreateMessage(roomId, content, senderId) async {
     await chatMessageRepository.createMessage(roomId, content, senderId);
   }
+
+  // 채팅방 참여 유저 데이터 가져오기
+  Future<void> fetchUsers(String roomId) async {
+    try {
+      // 참여 유저 ID 가져오기
+      final chatUsers = await chatMessageRepository.fetchUsersArray(roomId);
+      if (chatUsers == null) return;
+
+      // 참여 유저 정보 가져오기
+      final usersData = await chatMessageRepository.fetchUsersData(chatUsers);
+
+      if (usersData != null) {
+        users = usersData; // users 상태 업데이트
+      }
+    } catch (e) {
+      print('Error fetching users: $e');
+    }
+  }
+
+  // 채팅방 메시지와 유저 정보를 결합할 수 있는 새로운 클래스 생성
+  List<ChatMessageWithUser> get chatMessagesWithUser => state;
 }
 
 final chatViewModel = NotifierProvider.autoDispose
-    .family<ChatViewModel, List<ChatMessage>, String>(
+    .family<ChatViewModel, List<ChatMessageWithUser>, String>(
   () => ChatViewModel(),
 );
