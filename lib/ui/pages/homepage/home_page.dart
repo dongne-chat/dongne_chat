1b99992907e1f70fbd238c176168017ea918bf3d
chabugon <qnrhs0913@gmail.com>
2024-12-17 10:16:05 +0900

    [feat] 비밀번호 안보이게 수정, 메인화면에 톡리스트 보이게수정

--- lib/ui/pages/homepage/home_page.dart
+++ lib/ui/pages/homepage/home_page.dart
@@ -1,8 +1,52 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dongne_chat/ui/pages/chat/chat_page.dart';
 import 'package:dongne_chat/ui/pages/homepage/widget/home_page_chat_list.dart';
+import 'package:dongne_chat/ui/widgets/create_chat_room_button.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String? userId;
+  Future<List<DocumentSnapshot>>? chatRoomsFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadUserInfo();
+  }
+
+  Future<void> _loadUserInfo() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      userId = prefs.getString('userId');
+    });
+    if (userId != null) {
+      setState(() {
+        chatRoomsFuture = _fetchChatRooms(userId!);
+      });
+    }
+  }
+
+  Future<List<DocumentSnapshot>> _fetchChatRooms(String userId) async {
+    try {
+      final querySnapshot = await FirebaseFirestore.instance
+          .collection('chatRooms')
+          .where('users',
+              arrayContains: userId) // 'users' 필드에 userId가 포함된 문서 검색
+          .get();
+      return querySnapshot.docs;
+    } catch (e) {
+      print("Firestore 쿼리 실패: $e");
+      return [];
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,10 +77,9 @@ class HomePage extends StatelessWidget {
               borderRadius: BorderRadius.circular(20),
               color: Color(0xff8CA9CD),
             ),
-            
             child: Center(
               child: Text(
-                "아이디",
+                userId ?? "",
                 style: TextStyle(fontSize: 24),
               ),
             ),
@@ -54,13 +97,42 @@ class HomePage extends StatelessWidget {
             style: TextStyle(fontSize: 24),
           ),
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ListView(
-                children: [
-                  homepagechatlist(),
-                  ],
-              ),
+            child: FutureBuilder<List<DocumentSnapshot>>(
+              future: chatRoomsFuture,
+              builder: (context, snapshot) {
+                if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                  return Center(child: Text("참여중인 채팅방이 없습니다."));
+                } else {
+                  final chatRooms = snapshot.data!;
+                  return ListView.builder(
+                    itemCount: chatRooms.length,
+                    itemBuilder: (context, index) {
+                      final chatRoom = chatRooms[index];
+                      final chatRoomName = chatRoom['title'] ?? '채팅방 이름 없음';
+                      return GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => ChatPage(
+                                roomId: chatRoom.id,
+                                title: chatRoomName,
+                                userId: userId!,
+                              ),
+                            ),
+                          );
+                        },
+                        child: homepagechatlist(
+                          roomId: chatRoom.id,
+                          title: chatRoomName,
+                          category: chatRoom["category"],
+                          userCount: chatRoom['users'].length,
+                        ),
+                      );
+                    },
+                  );
+                }
+              },
             ),
           )
         ],
@@ -68,5 +140,3 @@ class HomePage extends StatelessWidget {
     );
   }
 }
-
-
